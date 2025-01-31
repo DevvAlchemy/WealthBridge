e112c42f8f5b56c6af31c08ab1004f23ca99740f
Carlito Kabambi <carlito.kabambi@triosstudent.com>
2025-01-31 11:45:22 -0500

    Initial Commit

---
+++ WealthBridge/WealthBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealthBridgeApp.swift
+//  WealthBridge
+//
+//  Created by Royal K on 2025-01-31.
+//
+
+import SwiftUI
+
+@main
+struct WealthBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WealthBridgeTests/WealthBridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WealthBridgeTests.swift
+//  WealthBridgeTests
+//
+//  Created by Royal K on 2025-01-31.
+//
+
+import Testing
+@testable import WealthBridge
+
+struct WealthBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
