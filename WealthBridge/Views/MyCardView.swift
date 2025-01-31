91e54d8120fc1c5bcf20bb895acbe90071ee4fbd
Carlito Kabambi <carlito.kabambi@triosstudent.com>
2025-01-31 14:31:41 -0500

    file structure

---
+++ WealthBridge/Views/MyCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MyCardView.swift
+//  WealthBridge
+//
+//  Created by Royal K on 2025-01-31.
+//
+
+import SwiftUI
+
+struct MyCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCardView()
+}
---
+++ WealthBridge/Views/TransferView.swift
@@ -0,0 +1,18 @@
+//
+//  TransferView.swift
+//  WealthBridge
+//
+//  Created by Royal K on 2025-01-31.
+//
+
+import SwiftUI
+
+struct TransferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransferView()
+}
